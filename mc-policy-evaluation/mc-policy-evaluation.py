c68962582717ae0e4de5defbeabb1670f2ebe703
Ralph Dugue <rdugue1@gmail.com>
2026-03-21 22:36:17 -0400

    Update solution for mc-policy-evaluation

--- mc-policy-evaluation/mc-policy-evaluation.py
+++ mc-policy-evaluation/mc-policy-evaluation.py
@@ -5,23 +5,22 @@ def mc_policy_evaluation(episodes, gamma, n_states):
     Returns: V (NumPy array of shape (n_states,))
     """
     # Write code here
-    T = len(episodes)
     V = np.zeros(n_states)
-    R = np.zeros(n_states)
-    count = np.zeros(n_states)
+    g_sums = np.zeros(n_states)
+    g_counts = np.zeros(n_states)
 
     for episode in episodes:
-        S = [step[0] for step in episode]
         G = 0
+        S = [step[0] for step in episode]
+
         for t in reversed(range(len(episode))):
             state, reward = episode[t]
-            G = gamma * G + reward
+            G = reward + gamma * G
             if state not in S[:t]:
-                R[state] += G
-                count[state] += 1
-                
-    for state in range(n_states):
-        if count[state] > 0:
-            V[state] = R[state] / count[state]
-        
+                g_sums[state] += G
+                g_counts[state] += 1
+
+    V = np.divide(g_sums, g_counts, out=np.zeros_like(g_sums), where=g_counts!=0)
     return V
+                
+            
