9a2b5f380e5a3a380de55b2a7a179c354463255c
Ralph Dugue <rdugue1@gmail.com>
2026-05-10 02:19:56 -0400

    Add solution for xavier-initialization

---
+++ xavier-initialization/xavier-initialization.py
@@ -0,0 +1,9 @@
+import numpy as input
+
+def xavier_initialization(W, fan_in, fan_out):
+    """
+    Scale raw weights to Xavier uniform initialization.
+    """
+    W = np.array(W, dtype=float)
+    L = np.sqrt(6 / (fan_in + fan_out))
+    return W * 2 * L - L
